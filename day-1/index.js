a2649a0c10edd703510aadb7cb641fd26a63cf7a
heenasavaliya <heenasavaliya331@gmail.com>
2024-10-01 13:50:08 +0530

    first commit

---
+++ day-1/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const port = 8001;
+
+const portHandler =(req,res) => {
+    res.write("<h1>server started </h1>")
+    res.end();
+}
+
+let server = http.createServer(portHandler);
+server.listen(port,(err)=>{
+    err ? console.log(err) : console.log("server started on port" + port);
+})
